9f47b32625c672fd64b9f89490f04c04fee5884b
yoshikoba <dev.mac.kazukoba@gmail.com>
2020-10-20 16:05:59 +0900

    1.簡単なリスト表示

---
+++ android/app/src/main/kotlin/com/wlteralex/flutter_listview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wlteralex.flutter_listview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ListViewSample());
+
+class ListViewSample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ListState();
+  }
+}
+
+class ListState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("List Test"),),
+      body: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Text(index.toString());
+          }),
+    );
+  }
+}
